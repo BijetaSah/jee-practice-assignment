20dcf67c3cee71f20d51e4681474d1568dd1cf8d
Bijeta Sah <bijetasah30@gmail.com>
2025-11-27 15:43:02 +0530

    feat : create a reuable button

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
+type ButtonProps = {
+  text?: string;
+  onClick?: () => void;
+};
 
-function Button() {
+function Button({ text = "Start Preparation", onClick }: ButtonProps) {
+  const baseStyle = `text-white bg-blue-600 cursor-pointer hover:bg-white hover:text-blue-600 border-2 border-blue-600 rounded-lg text-sm px-4 transition-colors duration-300 py-2 font-semibold sm:text-base`;
+  if (onClick)
+    return (
+      <button className={`${baseStyle}`} onClick={onClick}>
+        {text}
+      </button>
+    );
+
   return (
-    <Link
-      href={"/dashboard"}
-      className="text-white bg-blue-600 cursor-pointer hover:bg-white hover:text-blue-600 border-2 border-blue-600 rounded-lg text-sm px-4 transition-colors duration-300 py-2 font-semibold sm:text-base"
-    >
-      Start preparation
+    <Link href={"/dashboard"} className={`${baseStyle}`}>
+      {text}
     </Link>
   );
 }
